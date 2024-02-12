d60627dc1019edab10c49f58d295704be48bfe8f
alexjmeyer92 <alex.j.meyer15@gmail.com>
2024-02-11 21:38:54 -0600

    icons + kinda responsive-ish nav

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Template repo for quickly spinning up a full stack react app
 * Frontend
     * React w/ vite
     * Shadcn UI
-        * https://frontendshape.com/post/create-responsive-navbar-in-next-13-with-shadcn-ui
+        * Maybe fix responsive by just conditionally rendering a drop down menu button when MD or lower... Otherwise static nav
+            * bc current styling approach is pretty trash
         * 
     * PWA Features Out Of Box
         * manifest.json
@@ -14,3 +15,24 @@ Template repo for quickly spinning up a full stack react app
     * Python FastAPI
 * Database
     * Postgres
+
+
+```html
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+```
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,97 @@
 import './globals.css'
-import { PersonIcon } from '@radix-ui/react-icons'
 
 import { Button } from "@/components/ui/button"
 
-import { ThemeProvider } from './contexts/ThemeContext'
+// import { ThemeProvider } from './contexts/ThemeContext'
 import ThemeToggle from './components/ThemeMenu'
+import { useTheme } from './contexts/ThemeContext'
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
 
 function App() {
   // dashboard page example
   // https://github.com/shadcn-ui/ui/blob/main/apps/www/app/examples/dashboard/page.tsx
   // use radix react icons
+  const { theme } = useTheme();
+  // console.log(theme);
+  const [navState, setNavState] = useState(false);
+
+  const menuItems = [
+    { title: "Google Link", path: "https://google.com" },
+    { title: "Target Link", path: "https://target.com" },
+    { title: "Spotify Link", path: "https://spotify.com" }
+  ]
 
   return (
-    <ThemeProvider>
-      <>
-        <div className="border-b">
-          <nav
-            className={"flex place-items-center justify-between p"}
-          >
-            <h1 className="text-5xl pl-3">My Navbar</h1>
-            <div className="pr-3">
-              <ThemeToggle />
+    // <ThemeProvider>
+    <>
+      <nav
+        className="border-b w-full"
+      >
+        <div className="items-center justify-between px-1 max-w-screen-xl mx-auto md:flex md:px-8">
+          <div className="flex items-center justify-between md:block">
+            {theme === 'light' ? (
+              <img
+                src="turbo-sketch.svg"
+                alt="Turbo Charger"
+                width="85px"
+                height="85px"
+              />
+            ) : (
+              <img
+                src="turbo-sketch-white.svg"
+                alt="Turbo Charger"
+                width="85px"
+                height="85px"
+              />
+            )}
+            <div className="md:hidden">
+              <Button variant="outline" onClick={() => setNavState(!navState)}>
+                <HamburgerMenuIcon />
+              </Button>
             </div>
+          </div>
 
-          </nav>
-        </div>
-
-        <div className="grid place-items-center h-screen">
-          <Button
-            size="lg"
-            variant="outline"
-            onClick={() => console.log("Clicking the Button")}
+          <div
+            className={`pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navState ? "block" : "hidden"
+              }`}
           >
-            Click me
-            <PersonIcon />
-          </Button>
+            {menuItems.map((item, idx) => (
+              <Button variant="ghost">
+                <a href={item.path}>{item.title}</a>
+              </Button>
+            ))}
+            {/* <ThemeToggle /> */}
+          </div>
+          <div className={`md:block md:pb-0 md:mt-0 ${navState ? "block" : "hidden"
+            }`}>
+            <ThemeToggle />
+          </div>
         </div>
-      </>
-    </ThemeProvider>
+
+
+      </nav>
+
+      <div className="grid place-items-center h-screen">
+
+        {theme === 'light' ? (
+          <img
+            src="turbo-sketch.svg"
+            alt="Turbo Charger"
+            width="250px"
+            height="250px"
+          />
+        ) : (
+          <img
+            src="turbo-sketch-white.svg"
+            alt="Turbo Charger"
+            width="250px"
+            height="250px"
+          />
+        )}
+      </div>
+    </>
+    // </ThemeProvider>
   )
 }
 
