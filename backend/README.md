61a8315a4bf0d2ebbaf48f6460c2b71232d9ec1e
alexjmeyer92 <alex.j.meyer15@gmail.com>
2024-02-18 10:55:48 -0600

    barely an API

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+# Hot Turbo Backend
+
+* Rest Service using python + FastAPI
+    * To use install `python3.12.x` & `poetry`
+        * run `poetry install` in this directory to install dependencies
+
---
+++ backend/config.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from os import getenv
+import logging
+
+app_logger = logging.getLogger()
+
+logging.basicConfig(
+    format="[%(levelname)s:%(asctime)s] %(message)s", level=logging.DEBUG
+)
+# try to load .env.local file for local dev
+# otherwise we will grab configs from env vars set by docker compose
+load_dotenv(dotenv_path="../.env.local")
+
+# Get app environment
+APP_ENV = getenv("APP_ENV")
+app_logger.info(f"Running API in {APP_ENV} mode")
+
+# Frontend info
+DOMAIN_FRONTEND = getenv("DOMAIN_FRONTEND")
+
+BACKEND_CORS_ORGINS = [f"http://{DOMAIN_FRONTEND}", f"https://{DOMAIN_FRONTEND}"]
+
+# DB Info
+POSTGRES_USER = getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = getenv("POSTGRES_PASSWORD")
+DB_URL = getenv("DB_URL")
+
+# JWT
+JWT_AUTH_SECRET = getenv("JWT_AUTH_SECRET")
